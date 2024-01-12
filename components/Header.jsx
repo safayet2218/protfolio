49fd4b6502e81ca7ed4291711ec0b8eb28a86a5f
safayet2218 <safayet2218@gmail.com>
2024-01-12 16:55:15 +0600

    update

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,31 @@
+"use client";
+import React, { useState, useEffect } from "react";
 import ThemeToggler from "./ThemeToggler";
+import Logo from "./Logo";
+import Nav from "./Nav";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
     <header>
-      <ThemeToggler />
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <Logo />
+          <div className="flex items-center gap-x-6">
+            {/* nav */}
+            <Nav
+              containerStyles="hidden xl:flex gap-x-8 items-center"
+              linkStyles="relative hover:text-primary transition-all"
+              underlineStyles="absolute left-0 top-full h-[2px] bg-primary w-full"
+            />
+            <ThemeToggler />
+          </div>
+          {/* mobile nav */}
+          <div className="xl:hidden">
+            <MobileNav />
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
